b0f0b6ec9039703e36b7f52249f3fc5de47658e8
falgun bokde <falgunbokade@gmail.com>
2022-11-09 14:42:36 +0530

    Added BillPayment Service Interface

---
+++ QuickPay/src/main/java/com/quickPay/service/BillPaymentServices.java
@@ -0,0 +1,9 @@
+package com.quickPay.service;
+
+public interface BillPaymentServices {
+
+}
+
+
+// addBillPayment(BillPayment billpayment ) return type wallet and throws BillPaymentException , WalletException 
+//viewBillPayment(BillPayment billpayment) return type wallet and throws BillPaymentException , WalletException 
