ef76e6faa8ce68d9d92f0dbc8ef4dc4eec26122f
Ravishek Singh <ravishingsingh1@gmail.com>
2022-11-09 19:23:14 +0530

    Added Benificiary Service Interface

---
+++ QuickPay/src/main/java/com/quickPay/service/BenificiaryService.java
@@ -0,0 +1,18 @@
+package com.quickPay.service;
+
+public interface BenificiaryService {
+
+	/* 
+	 
+	 addBenificiary(BenificiaryDetails bd ) return type BenificiaryDetails and throws BankAccountException 
+	 
+	deleteBenificiary(BenificiaryDetails bd ) return type BenificiaryDetails and throws BankAccountException 
+	 
+	 viewBenificiary(String mobno)  return type BenificiaryDetails and throws BankAccountException
+	 
+	 viewAllBenificiary(Customer customer)  return type List<Benificiary> and throws BankAccountException 
+	 
+	*/
+}
+
+
