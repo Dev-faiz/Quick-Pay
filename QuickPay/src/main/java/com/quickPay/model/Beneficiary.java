456538e32bd15aa12085031b4c9f88a81e7c2a69
Ravishek Singh <ravishingsingh1@gmail.com>
2022-11-12 02:29:04 +0530

    Completed Beneficiary Controller Work.

--- QuickPay/src/main/java/com/quickPay/model/Beneficiary.java
+++ QuickPay/src/main/java/com/quickPay/model/Beneficiary.java
@@ -1,11 +1,11 @@
 package com.quickPay.model;
 
-
-
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,22 +14,57 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Data
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Beneficiary {
-	
-	@Id
-	private Integer  beneficiaryId ;
-	private String beneficiaryName ;
-	private String mobileNumber ; 
-	
-	@ManyToOne
-	private Wallet wallet ; 
-	
-	
-	
-}
 
+
+	@Size(min = 2, max = 10, message = "Name length min=2 and max=10")
+	@NotNull(message = "Name is mandatory")
+	private String beneficiaryName;
+
+	@Id
+	@NotNull(message = "MobileNumber is mandatory")
+	private String mobileNumber;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Wallet wallet;
+
+	public Beneficiary() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Beneficiary(
+			@Size(min = 2, max = 10, message = "Name length min=2 and max=10") @NotNull(message = "Name is mandatory") String beneficiaryName,
+			@NotNull(message = "MobileNumber is mandatory") String mobileNumber, Wallet wallet) {
+		super();
+		this.beneficiaryName = beneficiaryName;
+		this.mobileNumber = mobileNumber;
+		this.wallet = wallet;
+	}
+
+	public String getBeneficiaryName() {
+		return beneficiaryName;
+	}
+
+	public void setBeneficiaryName(String beneficiaryName) {
+		this.beneficiaryName = beneficiaryName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Wallet getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+
+
+
+}
